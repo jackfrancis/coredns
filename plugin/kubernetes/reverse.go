df9aaef0e1e593052150e1faf8eddf9e789698af
Jack Francis <jackfrancis@gmail.com>
2025-03-06 11:55:09 -0800

    return PTR record if pod hostname is not set
    Signed-off-by: Jack Francis <jackfrancis@gmail.com>

--- plugin/kubernetes/reverse.go
+++ plugin/kubernetes/reverse.go
@@ -37,24 +37,37 @@ func (k *Kubernetes) serviceRecordForIP(ip, name string) []msg.Service {
 		return []msg.Service{{Host: domain, TTL: k.ttl}}
 	}
 	// If no cluster ips match, search endpoints
-	var svcs []msg.Service
+	// Keep a discrete accounting of services with Hostnames set and those without
+	// So that we don't return a mixed set of service records with and without Hostnames set
+	var svcsHostname, svcsIPs []msg.Service
 	for _, ep := range k.APIConn.EpIndexReverse(ip) {
 		if len(k.Namespaces) > 0 && !k.namespaceExposed(ep.Namespace) {
 			continue
 		}
 		for _, eps := range ep.Subsets {
 			for _, addr := range eps.Addresses {
-				// The endpoint's Hostname will be set if this endpoint is supposed to generate a PTR.
-				// So only return reverse records that match the IP AND have a non-empty hostname.
-				// Kubernetes more or less keeps this to one canonical service/endpoint per IP, but in the odd event there
-				// are multiple endpoints for the same IP with hostname set, return them all rather than selecting one
-				// arbitrarily.
-				if addr.IP == ip && addr.Hostname != "" {
-					domain := strings.Join([]string{addr.Hostname, ep.Index, Svc, k.primaryZone()}, ".")
-					svcs = append(svcs, msg.Service{Host: domain, TTL: k.ttl})
+				var domain string
+				if addr.IP == ip {
+					if addr.Hostname != "" {
+						// If the endpoint's Hostname is set, compose a compliant PTR record composed from that hostname
+						// Kubernetes more or less keeps this to one canonical service/endpoint per IP, but in the odd event there
+						// are multiple endpoints for the same IP with hostname set, return them all rather than selecting one
+						// arbitrarily.
+						domain = strings.Join([]string{addr.Hostname, ep.Index, Svc, k.primaryZone()}, ".")
+						svcsHostname = append(svcsHostname, msg.Service{Host: domain, TTL: k.ttl})
+					} else {
+						// If the endpoint's Hostname is not set, generate a hostname from the IP address compose compliant PTR record
+						domain = strings.Join([]string{endpointHostname(addr, k.endpointNameMode), ep.Index, Svc, k.primaryZone()}, ".")
+						svcsIPs = append(svcsIPs, msg.Service{Host: domain, TTL: k.ttl})
+					}
 				}
 			}
 		}
 	}
-	return svcs
+	// If our service list with Hostnames set is not empty, return it
+	if len(svcsHostname) > 0 {
+		return svcsHostname
+	}
+	// Otherwise return the service list with IPs
+	return svcsIPs
 }
